01ee7ae6d3adcb14e60befc06b6f23faf0d60fa9
reinvald <aavo@reinvald.com>
2024-07-12 12:31:37 -0400

    now fetching access token from spotify using dotnet secrets

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class SpotifyWrapped
+{
+    private static HttpClient sharedClient = new()
+    {
+        BaseAddress = new Uri("https://jsonplaceholder.typicode.com"),
+    };
+
+    static async Task Main(string[] args)
+    {
+        await SpotifyController.RequestAccessTokenAsync(sharedClient);
+    }
+}
---
+++ SpotifyController.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+class SpotifyController
+{
+
+    private static string ?_clientId;
+    private static string ?_clientSecret;
+    
+    public static async Task RequestAccessTokenAsync(HttpClient client)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<SpotifyController>()
+            .Build();
+
+        _clientId = configuration["spotifyClientId"];
+        _clientSecret = configuration["spotifyClientSecret"];
+
+        if (_clientId is not null & _clientSecret is not null) {
+            var contentMap = new Dictionary<string, string>
+            {
+                { "client_id", _clientId },
+                { "client_secret", _clientSecret },
+                { "grant_type", "client_credentials" }
+            };
+
+            var content = new FormUrlEncodedContent(contentMap);
+            HttpResponseMessage response = await client.PostAsync("https://accounts.spotify.com/api/token", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Console.Write(responseString);
+        }
+
+    }
+}
